8dd63bdd84e998630c9693a477506ebc8d2d7a52
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-20 18:40:40 +0000

    feat: add community module to schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,5 @@
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import { userSchema } from './modules/User';
+import { communitySchema } from './modules/Community';
 
-export default buildSubgraphSchema([userSchema]);
+export default buildSubgraphSchema([userSchema, communitySchema]);
