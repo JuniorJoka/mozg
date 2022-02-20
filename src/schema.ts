6b66a0edcddddfc0baa7ba1c1f33dfa03347e7de
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-20 22:00:52 +0000

    feat: add follower to schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,10 @@
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import { userSchema } from './modules/User';
 import { communitySchema } from './modules/Community';
+import { followerSchema } from './modules/Follower';
 
-export default buildSubgraphSchema([userSchema, communitySchema]);
+export default buildSubgraphSchema([
+  userSchema,
+  communitySchema,
+  followerSchema,
+]);
