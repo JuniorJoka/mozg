e21caa74a4a38b513ef6ea2e92cb5387e7613b15
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-22 21:19:52 +0000

    test(auth): add tests for auth funcs

--- src/__tests__/User/auth.test.ts
+++ src/__tests__/User/auth.test.ts
@@ -2,7 +2,8 @@ import { JwtPayload } from 'jsonwebtoken';
 import db from '../../utils/testdb';
 import { newViewer } from '../../utils/testhelpers';
 
-import { user } from '../../modules/User/utils/auth';
+import { Password, user } from '../../modules/User/utils/auth';
+import faker from '@faker-js/faker';
 
 beforeAll(async () => db.connect());
 afterAll(async () => db.disconnect());
@@ -10,9 +11,8 @@ afterEach(async () => db.clearDatabase());
 
 let logged: { [key: string]: string };
 
-beforeEach(async () => (logged = await newViewer()));
-
 describe('Auth', () => {
+  beforeEach(async () => (logged = await newViewer()));
   describe('LoggedIn User', () => {
     it('returns logged in user on valid token', async () => {
       const tokenuser = user.get(logged.token) as JwtPayload;
@@ -22,6 +22,38 @@ describe('Auth', () => {
     it('returns null on invalid token', async () => {
       const tokenUser = user.get('sometoken');
       expect(tokenUser).toBeNull();
+    });
+
+    it('validates user from context on valid id', async () => {
+      const id = user.validate({ user: { id: logged.id } });
+      expect(id).toBe(id);
+    });
+
+    it('throws error on invalid user', () => {
+      expect(() => user.validate({ user: null })).toThrow();
+    });
+
+    describe('Password', () => {
+      it('correctly hashes password', async () => {
+        const password = faker.internet.password();
+        const hashed = await Password.encrypt(password);
+        expect(hashed).not.toBe(password);
+      });
+
+      it('correctly verifies password', async () => {
+        const password = faker.internet.password();
+        const hashed = await Password.encrypt(password);
+        const passwordMatches = Password.verify(hashed, password);
+        expect(passwordMatches).toBeTruthy();
+      });
+
+      it('should not pass a wrong password', async () => {
+        const password = faker.internet.password();
+        const hashed = await Password.encrypt(password);
+        const password2 = faker.internet.password();
+        const passwordMatches = await Password.verify(hashed, password2);
+        expect(passwordMatches).toBeFalsy();
+      });
     });
   });
 });
