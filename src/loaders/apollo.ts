037d9354f7aa8e35be4b45ef53a7c509317b8860
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-04-26 21:14:55 +0000

    feat(loaders): format response and apollo request context

--- src/loaders/apollo.ts
+++ src/loaders/apollo.ts
@@ -1,18 +1,93 @@
 import { ApolloServer } from 'apollo-server-express';
 import { Express } from 'express';
-import { user } from '../modules/User/utils/auth';
+import { v4 } from 'uuid';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 import schema from '../schema';
+
+export const refreshTokens: Record<string, string | JwtPayload> = {};
+
+const corsOptions = {
+  origin: config.accessOrigin,
+  methods: 'GET, HEAD, POST, PUT, DELETE , PATCH',
+  credentials: true,
+};
 
 export default async (app: Express) => {
   const server = new ApolloServer({
     schema,
+
+    formatResponse: (response, requestContext) => {
+      // return response;
+      // force return 401 on errors which are not related to bad credentials i.e password
+      if (response.errors && !requestContext.request.variables?.password) {
+        if (requestContext.response?.http) {
+          requestContext.response.http.status = 401;
+        }
+      } else if (response.data?.login || response.data?.refresh) {
+        const tokenExpireDate = new Date();
+        tokenExpireDate.setDate(
+          tokenExpireDate.getDate() + 60 * 60 * 24 * 7 // 7 days
+        );
+
+        const refreshTokenId = v4();
+
+        const token = jwt.verify(
+          response.data?.login || response.data?.refresh,
+          config.jwtSecret
+        ) as unknown as { data: any };
+
+        refreshTokens[refreshTokenId] = token.data;
+        const refreshToken = jwt.sign(
+          { data: refreshTokenId },
+          config.jwtSecret,
+          {
+            expiresIn: '7 days',
+          }
+        );
+
+        requestContext.response?.http?.headers.append(
+          'Set-cookie',
+          `refreshToken=${refreshToken}; expires=${tokenExpireDate}`
+        );
+      }
+
+      return response;
+    },
+
     context: ({ req }) => {
-      const token = req.headers.authorization;
-      let logUser = user.get(token || '');
-      return { user: logUser };
+      const ctx: {
+        user: jwt.JwtPayload | string;
+        refreshToken?: string;
+      } = {
+        user: '',
+      };
+
+      const cookies: { [key: string]: string } = {};
+      const cookieRaw = req.headers?.cookie ?? '';
+      if (cookieRaw) {
+        for (let cookie of cookieRaw.split(';')) {
+          const [name, value] = cookie.split('=');
+          cookies[name.trim()] = value.trim();
+        }
+      }
+
+      ctx.refreshToken = cookies?.refreshToken;
+
+      try {
+        if (req.headers['x-access-token']) {
+          const token = jwt.verify(
+            req.headers['x-access-token'] as string,
+            config.jwtSecret
+          ) as unknown as { data: object };
+          ctx.user = token.data;
+        }
+      } catch (e) {}
+
+      return ctx;
     },
   });
   await server.start();
-  server.applyMiddleware({ app, path: '/' });
+  server.applyMiddleware({ app, path: '/', cors: corsOptions });
   return server;
 };
