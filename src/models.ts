35ff47627559fe5728baf33503b6cdb81f922cc9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-05-01 23:34:07 +0000

    feat(models): add models

---
+++ src/models.ts
@@ -0,0 +1,15 @@
+import CommentModel from './modules/Comment';
+import CommunityModel from './modules/Community';
+import FollowerModel from './modules/Follower';
+import JoinModel from './modules/Join';
+import PostModel from './modules/Post';
+import UserModel from './modules/User';
+
+export default {
+  Comment: CommentModel,
+  Community: CommunityModel,
+  Follower: FollowerModel,
+  Join: JoinModel,
+  Post: PostModel,
+  User: UserModel,
+};
