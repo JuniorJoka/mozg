ae2fbca351ebc7b6ee054d96c51e3d96734ea78c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-05-05 00:17:32 +0000

    feat: move models into modules dir

---
+++ src/modules/index.ts
@@ -0,0 +1,15 @@
+import Comment from './Comment';
+import Community from './Community';
+import Follower from './Follower';
+import Join from './Join';
+import Post from './Post';
+import User from './User';
+
+export default {
+  Comment,
+  Community,
+  Follower,
+  Join,
+  Post,
+  User,
+};
