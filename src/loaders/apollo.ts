7f73f4df9fb58f663e0498db13263bcc17eb002c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-05-06 21:34:25 +0000

    refactor: pass models through context

--- src/loaders/apollo.ts
+++ src/loaders/apollo.ts
@@ -1,9 +1,9 @@
 import { ApolloServer } from 'apollo-server-express';
 import { Express } from 'express';
-import { v4 } from 'uuid';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import schema from '../schema';
+import models from '../modules';
 
 export const refreshTokens: Record<string, string | JwtPayload> = {};
 
@@ -13,80 +13,80 @@ const corsOptions = {
   credentials: true,
 };
 
+interface Context {
+  models: typeof models;
+  user: JwtPayload | string;
+}
+
 export default async (app: Express) => {
   const server = new ApolloServer({
     schema,
 
-    formatResponse: (response, requestContext) => {
-      // return response;
-      // force return 401 on errors which are not related to bad credentials i.e password
-      if (response.errors && !requestContext.request.variables?.password) {
-        if (requestContext.response?.http) {
-          requestContext.response.http.status = 401;
-        }
-      } else if (response.data?.login || response.data?.refresh) {
-        const tokenExpireDate = new Date();
-        tokenExpireDate.setDate(
-          tokenExpireDate.getDate() + 60 * 60 * 24 * 7 // 7 days
-        );
+    // formatResponse: (response, requestContext) => {
+    //   // return response;
+    //   // force return 401 on errors which are not related to bad credentials i.e password
+    //   if (response.errors && !requestContext.request.variables?.password) {
+    //     if (requestContext.response?.http) {
+    //       requestContext.response.http.status = 401;
+    //     }
+    //   } else if (response.data?.login || response.data?.refresh) {
+    //     const tokenExpireDate = new Date();
+    //     tokenExpireDate.setDate(
+    //       tokenExpireDate.getDate() + 60 * 60 * 24 * 7 // 7 days
+    //     );
 
-        const refreshTokenId = v4();
+    //     const refreshTokenId = v4();
 
-        const token = jwt.verify(
-          response.data?.login || response.data?.refresh,
-          config.jwtSecret
-        ) as unknown as { data: any };
+    //     const token = jwt.verify(
+    //       response.data?.login || response.data?.refresh,
+    //       config.jwtSecret
+    //     ) as unknown as { data: any };
 
-        refreshTokens[refreshTokenId] = token.data;
-        const refreshToken = jwt.sign(
-          { data: refreshTokenId },
-          config.jwtSecret,
-          {
-            expiresIn: '7 days',
-          }
-        );
+    //     refreshTokens[refreshTokenId] = token.data;
+    //     const refreshToken = jwt.sign(
+    //       { data: refreshTokenId },
+    //       config.jwtSecret,
+    //       {
+    //         expiresIn: '7 days',
+    //       }
+    //     );
 
-        requestContext.response?.http?.headers.append(
-          'Set-cookie',
-          `refreshToken=${refreshToken}; expires=${tokenExpireDate}`
-        );
-      }
+    //     requestContext.response?.http?.headers.append(
+    //       'Set-cookie',
+    //       `refreshToken=${refreshToken}; expires=${tokenExpireDate}`
+    //     );
+    //   }
 
-      return response;
-    },
+    //   return response;
+    // },
 
     context: ({ req }) => {
-      const ctx: {
-        user: jwt.JwtPayload | string;
-        refreshToken?: string;
-      } = {
-        user: '',
-      };
+      const ctx: Context = { user: '', models };
 
-      const cookies: { [key: string]: string } = {};
-      const cookieRaw = req.headers?.cookie ?? '';
-      if (cookieRaw) {
-        for (let cookie of cookieRaw.split(';')) {
-          const [name, value] = cookie.split('=');
-          cookies[name.trim()] = value.trim();
-        }
-      }
+      // const cookies: { [key: string]: string } = {};
+      // const cookieRaw = req.headers?.cookie ?? '';
+      // if (cookieRaw) {
+      //   for (let cookie of cookieRaw.split(';')) {
+      //     const [name, value] = cookie.split('=');
+      //     cookies[name.trim()] = value.trim();
+      //   }
+      // }
 
-      ctx.refreshToken = cookies?.refreshToken;
+      // ctx.refreshToken = cookies?.refreshToken;
 
-      try {
-        if (req.headers['x-access-token']) {
-          const token = jwt.verify(
-            req.headers['x-access-token'] as string,
-            config.jwtSecret
-          ) as unknown as { data: object };
-          ctx.user = token.data;
-        }
-      } catch (e) {}
-
+      // try {
+      //   if (req.headers['x-access-token']) {
+      //     const token = jwt.verify(
+      //       req.headers['x-access-token'] as string,
+      //       config.jwtSecret
+      //     ) as unknown as { data: object };
+      //     ctx.user = token.data;
+      //   }
+      // } catch (e) {}
       return ctx;
     },
   });
+
   await server.start();
   server.applyMiddleware({ app, path: '/', cors: corsOptions });
   return server;
