396fb853e78f2cbe71c754bcf467ee69a41b76ad
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-05-08 16:24:06 +0000

    fix(post): use camelcasing for model properties

--- src/modules/Post/mutationResolvers/createPost.ts
+++ src/modules/Post/mutationResolvers/createPost.ts
@@ -14,7 +14,7 @@ export default async (_: Object, args: newPostArgs, context: Context) => {
   const id = v4();
   await Post.create({
     id,
-    creator_id: context.user?.id,
+    creatorId: context.user?.id,
     communityId: commId,
     ...args,
   });
