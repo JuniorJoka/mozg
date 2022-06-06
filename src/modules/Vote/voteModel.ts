42d088d6da54fdfc2f9b3ed02f4f2cb240e84a22
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-06 23:09:59 +0000

    feat(vote): add vote module

---
+++ src/modules/Vote/voteModel.ts
@@ -0,0 +1,12 @@
+import { model, Model, Schema } from 'mongoose';
+import { v4 } from 'uuid';
+
+const voteSchema = new Schema({
+  _id: { type: String, required: true, alias: 'id', default: () => v4() },
+  userId: { type: String, required: true, ref: 'User' },
+  targetId: { type: String, required: true },
+  targetType: { type: String, required: true },
+  voteValue: { type: Number, required: true },
+});
+
+export default model('Vote', voteSchema);
