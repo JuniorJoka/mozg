87ebbaae45a8853d49591a2791fec913b60dd533
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-19 15:33:49 +0000

    feat(schema): add vote schema to schemas

--- src/schema.ts
+++ src/schema.ts
@@ -5,6 +5,7 @@ import { followSchema } from './modules/Follow';
 import { joinSchema } from './modules/Join/';
 import { postSchema } from './modules/Post';
 import { commentSchema } from './modules/Comment';
+import { voteSchema } from './modules/Vote';
 
 export default buildSubgraphSchema([
   userSchema,
@@ -13,4 +14,5 @@ export default buildSubgraphSchema([
   joinSchema,
   postSchema,
   commentSchema,
+  voteSchema
 ]);
