aed1870361687d2623f44e758ea4f7f8fbba8864
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-20 01:54:36 +0000

    fix(comment): return result after queryuey

--- src/modules/Comment/commentResolvers/replies.ts
+++ src/modules/Comment/commentResolvers/replies.ts
@@ -1,6 +1,6 @@
 import CommentType from '../commentType';
 import { Context } from '../../../shared/Types';
 
-export default async ({ _id }: CommentType, _: {}, { models }: Context) => {
-  await models.Comment.find({ parentId: _id });
+export default async (args: CommentType, _: {}, { models }: Context) => {
+  return await models.Comment.find({ parentId: args._id });
 };
