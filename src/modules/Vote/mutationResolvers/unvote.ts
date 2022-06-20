92b34c4f470b52a5cec4a93dc8d4ff107edbf2e2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-20 21:20:18 +0000

    feat(vote): update stats on vote

--- src/modules/Vote/mutationResolvers/unvote.ts
+++ src/modules/Vote/mutationResolvers/unvote.ts
@@ -5,5 +5,28 @@ import { Vote } from '../voteType'
 export default async (_: {}, args: Vote, { models, user }: Context) => {
   const { targetId } = args;
   const userId = user?.id;
-  await models.Vote.findOneAndRemove({ userId, targetId })
+  const postId = targetId
+  const commentId = targetId
+
+  const vote = await models.Vote.findOneAndRemove({ userId, targetId })
+
+  if (!vote) {
+    return
+  }
+  const { isUpVote, targetType } = vote
+
+  if (isUpVote) {
+    if (targetType === 'post') {
+      await models.PostStats.findOneAndUpdate({ postId }, { $inc: { upvotes: -1 } })
+    } else {
+      await models.CommentStats.findOneAndUpdate({ commentId }, { inc: { upvotes: -1 } })
+    }
+  } else {
+    if (targetType === 'post') {
+      await models.PostStats.findOneAndUpdate({ postId }, { $inc: { downvotes: -1 } })
+    } else {
+      await models.CommentStats.findOneAndUpdate({ commentId }, { inc: { downvotes: -1 } })
+    }
+
+  }
 }
--- src/modules/Vote/mutationResolvers/vote.ts
+++ src/modules/Vote/mutationResolvers/vote.ts
@@ -4,6 +4,8 @@ import { Vote } from '../voteType';
 export default async (_: {}, args: Vote, { models, user }: Context) => {
   const { targetId, targetType, isUpVote } = args;
   const userId = user?.id;
+  const postId = targetId
+  const commentId = targetId
 
   const hasVoted = await models.Vote.findOne({ userId, targetId })
   if (hasVoted) {
@@ -12,14 +14,48 @@ export default async (_: {}, args: Vote, { models, user }: Context) => {
         userId,
         targetId
       }, { $set: { isUpVote } })
+
+      if (targetType === 'post') {
+        if (isUpVote) {
+          await models.PostStats.findOneAndUpdate({ postId }, { $inc: { upvotes: 1 } })
+          await models.PostStats.findOneAndUpdate({ postId }, { $inc: { downvotes: -1 } })
+        } else {
+          await models.PostStats.findOneAndUpdate({ postId }, { $inc: { downvotes: 1 } })
+          await models.PostStats.findOneAndUpdate({ postId }, { $inc: { upvotes: -1 } })
+        }
+      } else {
+        if (isUpVote) {
+          await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { upvotes: 1 } })
+          await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { downvotes: -1 } })
+        } else {
+          await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { downvotes: 1 } })
+          await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { upvotes: -1 } })
+        }
+      }
     };
     return
   }
 
-  const vote = await models.Vote.create({
+  //  create vote instance
+  await models.Vote.create({
     userId,
     targetType,
     targetId,
     isUpVote,
   });
+
+
+  if (targetType === 'post') {
+    if (isUpVote) {
+      await models.PostStats.findOneAndUpdate({ postId }, { $inc: { upvotes: 1 } })
+    } else {
+      await models.PostStats.findOneAndUpdate({ postId }, { $inc: { downvotes: 1 } })
+    }
+  } else {
+    if (isUpVote) {
+      await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { upvotes: 1 } })
+    } else {
+      await models.CommentStats.findOneAndUpdate({ commentId }, { $inc: { downvotes: 1 } })
+    }
+  }
 };
